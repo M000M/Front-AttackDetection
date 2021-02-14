da7c5f9e4325278b6bffdc0fa9e1eb01664914c5
tancong <itancong@163.com>
2021-02-14 23:07:26 +0800

    02.14

--- src/components/logs/Logs.vue
+++ src/components/logs/Logs.vue
@@ -1,43 +1,63 @@
 <template>
     <div>
-        <div style="font-size: 25px; font-family: '宋体', cursive">所有日志</div>
-        <br>
+<!--        <div style="font-size: 25px; font-family: '宋体', cursive">所有日志</div>-->
+<!--        <br>-->
 
         <el-table
-            :data="tableData"
+            :data="logData"
             style="width: 100%"
             border
             :max-height="900">
-            <el-table-column
-                label="编号"
-                width="55">
-                <template slot-scope="scope">
-                    {{ scope.row.id }}
-                </template>
-            </el-table-column>
+            <el-table-column label="remoteAddr" prop="remoteAddr"></el-table-column>
 
-            <el-table-column label="容器名" prop="name"></el-table-column>
+            <el-table-column label="logTimestamp" prop="logTimestamp"></el-table-column>
 
-            <el-table-column label="宿主机" prop="host"></el-table-column>
+            <el-table-column label="user" prop="user"></el-table-column>
 
-            <el-table-column label="src_ip" prop="srcIp"></el-table-column>
+            <el-table-column label="param" prop="param"></el-table-column>
 
-            <el-table-column label="src_port" prop="srcPort"></el-table-column>
+            <el-table-column label="httpStatus" prop="httpStatus"></el-table-column>
 
-            <el-table-column label="dest_port" prop="destPort"></el-table-column>
+            <el-table-column label="uri" prop="uri"></el-table-column>
 
-            <el-table-column label="command" prop="command"></el-table-column>
-
-            <el-table-column label="start_time" prop="startTime"></el-table-column>
-
-            <el-table-column label="stop_time" prop="stopTime"></el-table-column>
+            <el-table-column label="bodyBytesSent" prop="bodyBytesSent"></el-table-column>
         </el-table>
     </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-    name: "Logs"
+    name: "Logs",
+
+    data() {
+        return {
+            logData: []
+        }
+    },
+
+    methods: {
+        findAllTableData() {
+            axios.get("http://127.0.0.1:9000/es/getAll").then(res => {
+                this.logData = res.data.data;
+            });
+        },
+
+        intervalFetchData: function () {
+            setInterval(() => {
+                this.findAllTableData();
+            }, 500);
+        }
+    },
+
+    mounted() {
+        this.intervalFetchData();
+    },
+
+    created() {
+        this.findAllTableData();
+    }
 }
 </script>
 
