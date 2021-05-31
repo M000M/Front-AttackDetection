3ab2f9c00b51748821a4a331467946d8a73e8d63
tancong <itancong@163.com>
2021-05-31 08:36:31 +0800

    05.31 macos

--- src/components/containers/DynamicDeployment.vue
+++ src/components/containers/DynamicDeployment.vue
@@ -1,32 +1,64 @@
 <template>
     <div>
         <div style="font-size: 25px; font-family: '宋体', cursive; margin-left: 40%">动态化部署</div>
-        <el-container>
-            <el-main>
-                <el-form :model="dynamicDeploymentForm" label-width="200px" ref="verificationLog">
+        <br>
+        <el-form :model="dynamicDeploymentForm" ref="verificationLog">
+            <el-row>
+                <el-col>
+                    <el-form-item label="是否开启动态化部署" prop="isDynamicSwitchOn">
+                        <el-switch
+                            v-model="isDynamicSwitchOn"
+                            active-color="#2E9AFE"
+                            inactive-color="#585858"
+                            @change="handleChange">
+                        </el-switch>
+                    </el-form-item>
+                </el-col>
+            </el-row>
+        </el-form>
+        <el-table
+            :data="tableData"
+            style="width: 100%"
+            border
+            :max-height="900"
+            v-if="isDynamicSwitchOn">
+            <el-table-column
+                label="编号"
+                width="100">
+                <template slot-scope="scope">
+                    {{ scope.$index + 1 }}
+                </template>
+            </el-table-column>
+
+            <el-table-column label="容器类型" prop="image"></el-table-column>
+
+            <el-table-column label="容器数量" prop="count"></el-table-column>
+
+            <el-table-column label="受到的攻击次数" prop="attackCount"></el-table-column>
+
+            <el-table-column label="攻击/数量设定">
+                <template slot-scope="scope">
                     <el-row>
-                        <el-col :span="20" :offset="1">
-                            <el-form-item label="是否开启动态部署">
-                                <el-switch
-                                    v-model="isDynamicSwitchOn"
-                                    active-color="#2E9AFE"
-                                    inactive-color="#585858">
-                                </el-switch>
-                            </el-form-item>
+                        <el-col :span="9" :offset="1">
+                            <el-input placeholder="最小值" style="width: 80px;" :value="scope.row.minRate"></el-input>
+                        </el-col>
+                        <el-col :span="9" :offset="3">
+                            <el-input placeholder="最大值" style="width: 80px" :value="scope.row.maxRate"></el-input>
                         </el-col>
                     </el-row>
-                    <el-row>
-                        <el-col :span="8" :offset="1">
-                            <el-form-item label="日志数据编号" prop="logId">
-                                <el-input  placeholder="请输入日志数据编号"></el-input>
-                            </el-form-item>
-                        </el-col>
-                    </el-row>
-                </el-form>
-            </el-main>
-        </el-container>
+                </template>
+            </el-table-column>
 
-
+            <el-table-column label="操作">
+                <template slot-scope="scope">
+                    <el-button
+                        type="info"
+                        circle
+                        @click="handleSet(scope.$index, scope.row)">设置
+                    </el-button>
+                </template>
+            </el-table-column>
+        </el-table>
     </div>
 </template>
 
@@ -36,7 +68,53 @@ export default {
     data() {
         return {
             isDynamicSwitchOn: true,
-            dynamicDeploymentForm: []
+            dynamicDeploymentForm: {},
+            tableData: [
+                {
+                    image: "cowrie",
+                    count: 1,
+                    attackCount: 10,
+                    minRate: 10,
+                    maxRate: 20
+                },
+                {
+                    image: "conpot",
+                    count: 1,
+                    attackCount: 10,
+                    minRate: 10,
+                    maxRate: 20
+                },
+                {
+                    image: "adbhoney",
+                    count: 1,
+                    attackCount: 10,
+                    minRate: 10,
+                    maxRate: 20
+                },
+                {
+                    image: "honeytrap",
+                    count: 1,
+                    attackCount: 10,
+                    minRate: 10,
+                    maxRate: 20
+                },
+                {
+                    image: "citrixhoneypot",
+                    count: 1,
+                    attackCount: 10,
+                    minRate: 10,
+                    maxRate: 20
+                }
+            ]
+        }
+    },
+    methods: {
+        handleChange() {
+            console.log(this.isDynamicSwitchOn);
+        },
+        handleSet(index, row) {
+            console.log(index);
+            console.log(row);
         }
     }
 }
